b582f7b8e444713b0656d7291fc321575ff0943a
willhalen <willhalen9@gmail.com>
2023-12-04 16:00:35 -0300

    Teste Dulino

---
+++ index.js
@@ -0,0 +1,37 @@
+
+// const http = require('http');
+
+//const hostname = '127.0.0.1';
+//const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type','text/plain');    
+//     res.end('Helo From William Santana')
+// });
+
+// server.listen(port,hostname, () => {
+//     console.log("servidor rodando!");
+// })
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+
+    res.sendFile(path.join(__dirname+'/index.html'));
+
+})
+
+router.get('/sobre',function(req,res){
+
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+
+})
+
+app.use('/',router);
+app.listen(process.env.port || 3000);
+
+console.log("server rodando");
+
